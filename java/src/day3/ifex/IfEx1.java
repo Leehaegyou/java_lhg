0f5422f0b11c78fe651640fb70744a1f7161e98a
Leehaegyou <134986451+Leehaegyou@users.noreply.github.com>
2023-06-01 14:56:46 +0900

    java if문 0판별 예제

---
+++ java/src/day3/ifex/IfEx1.java
@@ -0,0 +1,23 @@
+package day3.ifex;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		//정수 num가 0이면 0입니다. 라고 출력하는 코드를 작성하세요.
+		//정수 num가 0이다.
+		//정수 num가 0과 같다.
+		int num = 0;
+		if(num == 0) {
+			//0입니다. 라고 출력
+			System.out.println(num + " = 0입니다. ");
+		}
+		//정수 num가 0이 아니면 0이 아닙니다. 라고 출력하는 코드를 작성하세요.
+		//정수 num가 0이 아니다.
+		//정수 num가 0과 같지 않다.
+		if(num != 0) {
+			//0이 아닙니다. 라고 출력
+			System.out.println(num + " = 0이 아닙니다. ");
+		}
+	}
+
+}
