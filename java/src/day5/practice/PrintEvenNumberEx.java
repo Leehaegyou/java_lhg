a4a80cef3e38562a8725c5fe54a3fedfd130e41c
Leehaegyou <134986451+Leehaegyou@users.noreply.github.com>
2023-06-05 15:01:21 +0900

    java while문 짝수 출력 예제

---
+++ java/src/day5/practice/PrintEvenNumberEx.java
@@ -0,0 +1,13 @@
+package day5.practice;
+
+public class PrintEvenNumberEx {
+
+	public static void main(String[] args) {
+		//10이하의 짝수를 출력하는 코드를 작성하세요.
+		int i = 1;
+		while(i <= 5) {
+			System.out.println(i++ * 2);
+		}
+	}
+
+}
