b2996d427cf783c70c763bc0a83edd3da19446f6
Leehaegyou <134986451+Leehaegyou@users.noreply.github.com>
2023-06-05 15:02:30 +0900

    java while문 1~10 출력 예제

---
+++ java/src/day5/whileex/CountNumberEx.java
@@ -0,0 +1,14 @@
+package day5.whileex;
+
+public class CountNumberEx {
+
+	public static void main(String[] args) {
+		//1부터 10까지 출력하는 코드를 작성하세요.
+		
+		int i = 1;
+		while(i <= 10) {
+			System.out.println(i++);
+		}
+	}
+
+}
