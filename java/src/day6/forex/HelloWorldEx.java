a30991c926023494f08ac2af89fb585faa941dac
Leehaegyou <134986451+Leehaegyou@users.noreply.github.com>
2023-06-07 14:48:43 +0900

    java for문 사용 Hello World 5번 출력 예제

---
+++ java/src/day6/forex/HelloWorldEx.java
@@ -0,0 +1,16 @@
+package day6.forex;
+
+public class HelloWorldEx {
+
+	public static void main(String[] args) {
+		//for문을 이용하여 HelloWorld를 5번 출력하는 예제
+		
+		int i;
+		for(i = 1; i<= 5; i++) {
+			System.out.println("Hello World");
+		}
+		System.out.println("==========");
+		
+	}
+
+}
