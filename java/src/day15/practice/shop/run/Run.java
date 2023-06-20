a8afd2356ffd9f0968da3e9167c160e1977df2ed
Leehaegyou <134986451+Leehaegyou@users.noreply.github.com>
2023-06-20 14:52:13 +0900

    15일차

---
+++ java/src/day15/practice/shop/run/Run.java
@@ -0,0 +1,11 @@
+package day15.practice.shop.run;
+
+import day15.practice.shop.controller.ShopManager;
+
+public class Run {
+
+	public static void main(String[] args) {
+		ShopManager manager = new ShopManager();
+		manager.run();
+	}
+}
